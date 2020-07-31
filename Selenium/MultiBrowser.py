d045806ee8bfcab56fc293f77d32a5c744b3f7ae
saintsavon <jaylensavon@gmail.com>
2020-07-31 10:51:39 -0400

    Setting up Selenium at home

---
+++ Selenium/MultiBrowser.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path=r"C:\webdrivers\chromedriver.exe")
+#driver = webdriver.Firefox(executable_path=r"C:\webdrivers\geckodriver.exe")
+#driver = webdriver.Ie(executable_path=r"C:\webdrivers\IEDriverServer.exe")
+
+driver.get("https://www.amazon.com/CableMod-PRO-ModMesh-RMi-Cable/dp/B0799F4XWF/ref=sr_1_2?dchild=1&keywords=cablemod%2Bcorsair%2Brmx&qid=1596206319&s=electronics&sr=1-2&th=1/")
+
+print(driver.title)  # Title of the page
+
+driver.close()  # Closes browser
